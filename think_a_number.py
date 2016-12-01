4eed246cfbf31300cd3cbcb7d6d9d624f2f7c476
Balázs Máté <balazsm86@gmail.com>
2016-12-01 15:30:02 +0100

    Restructured program, new functions added

--- think_a_number.py
+++ think_a_number.py
@@ -2,51 +2,57 @@ import os
 import random
 
 
-def new_random_number():
+def initialize():
     guessed_number = str(random.randint(1000, 9999))
     list_of_nums = [guessed_number[num] for num in range(4)]
-    print("The guess is: %s" % guessed_number)
+    print_number(list_of_nums)
     return list_of_nums
 
 
 def get_input():
-    num_of_right_numbers = int(input("How many digits are on the correct position: "))
+    num_of_right_numbers = int(input("How many digits are in the correct position: "))
     return num_of_right_numbers
 
 
-def generate_random_list(num_of_right_numbers, list_of_nums):
-    temp_list = list_of_nums
-    good_position_list = []
-    # for i in range(num_of_right_numbers):
-
-    return list_of_nums
-
-
-def check_cases(num_of_right_numbers, list_of_nums):
-    original_list_of_nums = list_of_nums
-    good_num = {"1": None, "2": None, "3": None, "4": None}
-    if num_of_right_numbers == 0:
-        new_random_number()
-    if num_of_right_numbers == 1:
-        pass
-    if num_of_right_numbers == 2:
-        pass
-    if num_of_right_numbers == 3:
-        pass
-    if num_of_right_numbers == 4:
-        guessed_number = "".join(list_of_nums)
-        print("Your number is: %s", % guessed_number)
-    list_of_nums[0] = str(random.randint(0, 9))
+def print_number(list_of_nums):
     guessed_number = "".join(list_of_nums)
     print("The guess is: %s" % guessed_number)
-    # if get_input() < num_of_right_numbers:
+
+
+def new_numbers(list_of_nums, num_of_right_numbers):
+    new_numbers_index = []
+    new_numbers_index.append(random.randint(0, 3))
+    while len(new_numbers_index) < 4 - num_of_right_numbers:
+        index = random.randint(0, 3)
+        if index in new_numbers_index:
+            continue
+        new_numbers_index.append(index)
+
+    for i in new_numbers_index:
+        list_of_nums[i] = str(random.randint(0, 9))
     return list_of_nums
+
+
+def check_number(list_of_nums):
+    guessed = False
+    num_of_right_numbers = get_input()
+    if num_of_right_numbers == 4:
+        guessed = True
+        return guessed
+    new_numbers(list_of_nums, num_of_right_numbers)  # Generates the new digits
+    print_number(list_of_nums)
+    return guessed
 
 
 def main():
-    list_of_nums = new_random_number()
-    num_of_right_numbers = get_input()
-    check_cases(num_of_right_numbers, list_of_nums)
-
+    list_of_nums = initialize()
+    # final_number = [0, 0, 0, 0]
+    guessed = False
+    while True:
+        guessed = check_number(list_of_nums)
+        if guessed == True:
+            print("Win! The number is guessed!")
+            return
+        continue
 
 main()
